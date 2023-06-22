d1d51667e977034225bd4d7764ef3babcd064177
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-06-22 18:49:49 +0100

    Fixed issue with delete function

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ import { Tree } from "./modules/tree.js";
 
 let array = [1, 2, 3, 4, 5, 6, 7, 8];
 let bst = new Tree(array);
-bst.printTree();
+// bst.printTree();
 
 bst.insert(9);
 bst.insert(10);
 bst.printTree();
 
 bst.delete(9);
+bst.printTree();
+bst.delete(2);
+bst.delete(8);
+console.log(bst);
 bst.printTree();
