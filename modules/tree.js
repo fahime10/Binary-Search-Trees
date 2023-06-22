11c8395f52ac10616cd1289292e8620758c359a5
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-06-22 17:23:14 +0100

    Implemented insert and delete functions

--- modules/tree.js
+++ modules/tree.js
@@ -16,7 +16,6 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 export class Tree {
     constructor(array) {
         this.array = this.buildTree(array, 0, array.length - 1);
-        prettyPrint(this.array);
     }
 
     buildTree(array, start, end) {
@@ -32,6 +31,59 @@ export class Tree {
         root.right = this.buildTree(array, mid + 1, end);
 
         return root;
+    }
+
+    printTree() {
+      prettyPrint(this.array);
+    }
+
+    insert(value, array = this.array) {
+      if (array == null) {
+        return (array = new Node(value));
+      }
+
+      if (array.value < value) {
+        array.right = this.insert(value, array.right);
+      } else {
+        array.left = this.insert(value, array.left);
+      }
+
+      return array;
+    }
+
+    delete(value, array = this.array) {
+      if (array == null) {
+        return array;
+      }
+
+      if (array.value > value) {
+        array.left = this.delete(value, array.left);
+      } else if (array.value < value) {
+        array.right = this.delete(value, array.right);
+      } else {
+        if (array.left == null) {
+          return array.right;
+        } else if (array.right == null) {
+          return array.left;
+        }
+
+        array.value = minValue(array);
+        array.right = this.delete(array.right, array.value);
+      }
+
+      return array;
+    }
+
+    // helper function to supply the minimum value in the array
+    minValue(array) {
+      let min = array.value;
+
+      while (array != null) {
+        min = array.value;
+        array = array.left;
+      }
+
+      return min;
     }
     
 }
--- script.js
+++ script.js
@@ -2,3 +2,11 @@ import { Tree } from "./modules/tree.js";
 
 let array = [1, 2, 3, 4, 5, 6, 7, 8];
 let bst = new Tree(array);
+bst.printTree();
+
+bst.insert(9);
+bst.insert(10);
+bst.printTree();
+
+bst.delete(9);
+bst.printTree();
