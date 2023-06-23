ee542c056bdde56ba61b024227e05795317864fb
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-06-23 14:01:53 +0100

    Implemented isBalanced and rebalance functions

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ console.log("Preorder: " + bst.preorder());
 console.log("Postorder: " + bst.postorder());
 console.log("Height is " + bst.height());
 console.log("Depth of 8 is " + bst.depth(8));
+console.log("Is this tree balanced? " + bst.isBalanced());
+
+let array2 = [1, 7, 4, 23, 8, 4, 3, 5, 7, 9, 67, 6345, 324];
+let bst2 = new Tree(array2);
+
+bst2.insert(2);
+bst2.insert(34);
+bst2.insert(333);
+bst2.insert(4556);
+bst2.insert(6);
+bst2.insert(22);
+bst2.insert(3455);
+bst2.rebalance();
+bst2.printTree();
